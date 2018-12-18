b2e28c35c8b031e2bfa916cd73f0c942244574e2
Enrique Pinedo <enrjon@gmail.com>
2018-12-18 17:40:44 -0500

    moved main to core dir; added requirements.txt; updated config sections

---
+++ core/sentinelGet.py
@@ -0,0 +1,39 @@
+import os
+import requests
+import configparser
+#import HTTPDigestAuth
+
+# directories
+rawDir = "raw"
+tiffDir = "tiff"
+
+#os.chdir(workingDir)
+
+# credentials
+config = configparser.ConfigParser()
+config.read("foobar.ini")
+username = config['CREDENTIALS']['username']
+password = config['CREDENTIALS']['password']
+
+# url base
+baseURL = "https://scihub.copernicus.eu/dhus/search?q=*"
+
+#inital request to verify credentials
+from requests.auth import HTTPBasicAuth
+requests.get(baseURL, auth = HTTPBasicAuth(username, password))
+
+#### QUERY PARAMETERS ####
+
+# How far back to search; last 10 days
+dateFilter = "beginposition:[NOW-10DAYS TO NOW] AND endposition:[NOW-10DAYS TO NOW]"
+
+# result management
+paginationStart = 0 # pagination result position
+paginationRows = 20 # results per page(100 maximum)
+
+# product name (sentinel-2 data)
+platformFilter = "platformname:Sentinel-2"
+
+#print(baseURL)
+#print(timeCriteria)
+#print("working directory is " + os.getcwd())
