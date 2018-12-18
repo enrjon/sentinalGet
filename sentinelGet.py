db44b6b6155a14a004b8a03e1b00cbbc84c1efa5
Enrique Pinedo <enrjon@gmail.com>
2018-12-18 00:56:56 -0500

    added .gitignore for credentials and working directories

--- sentinelGet.py
+++ sentinelGet.py
@@ -1,7 +1,15 @@
 import os
-import urllib2
+import requests
+
+# directories
+workingDir = "SentinelGet"
+rawDir = "raw"
+tiffDir = "tiff"
+
+os.chdir(workingDir)
 
 # credentials
+creds = open("foobar.config")
 username = "testUser"
 password = "testPass"
 
@@ -9,12 +17,18 @@ password = "testPass"
 baseURL = "https://scihub.copernicus.eu/dhus/search?"
 
 
-# directories
-os.chdir("D:\Program "
+#### QUERY PARAMETERS ####
 
 # How far back to search; last 10 days
-timeCriteria = "beginposition:[NOW-10DAYS TO NOW] AND endposition:[NOW-10DAYS TO NOW]"
+dateFilter = "beginposition:[NOW-10DAYS TO NOW] AND endposition:[NOW-10DAYS TO NOW]"
 
-print(baseURL)
-print(timeCriteria)
-print("working directory is " + os.getcwd())
+# result management
+paginationStart = 0 # pagination result position
+paginationRows = 20 # results per page(100 maximum)
+
+# product name (sentinel-2 data)
+platformFilter = "platformname:Sentinel-2"
+
+#print(baseURL)
+#print(timeCriteria)
+#print("working directory is " + os.getcwd())
